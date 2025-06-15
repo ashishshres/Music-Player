fb69f0d8f30e03354e642268ca3fae810b171dfc
DharmendraWD <zaynhere@icloud.com>
2025-06-15 20:29:48 +0545

    working api

--- README.md
+++ README.md
@@ -1,12 +1,13 @@
-# React + Vite
+# Music Web App
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+Hi, this is a simple music web player made in **React.js** where you can search any song.  
+It fetches songs from the **Spotify API**.
 
-Currently, two official plugins are available:
+If you want to check it out, clone the repo, then run:
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+```bash
+npm i
+npm start
+```
 
-## Expanding the ESLint configuration
-
-If you are developing a production application, we recommend using TypeScript with type-aware lint rules enabled. Check out the [TS template](https://github.com/vitejs/vite/tree/main/packages/create-vite/template-react-ts) for information on how to integrate TypeScript and [`typescript-eslint`](https://typescript-eslint.io) in your project.
+![Alt text](./Capture.PNG)
--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,101 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
+import AlertBox from './Alert'
 
 function App() {
-  const [count, setCount] = useState(0)
+const [tracks, settracks] = useState([])
+const [userinput, setuserinput] = useState("")
+const [isLoading, setisLoading] = useState(false)
+
+
+const getTracks = async ()=>{
+  setisLoading(true)
+ let data= await fetch(`https://v1.nocodeapi.com/dharmendrawd/spotify/VashNOTgtKSzVSai/search?q=${userinput}&type=track`)
+ let convertedData = await data.json();
+ console.log(convertedData.tracks.items);
+ settracks(convertedData.tracks.items)
+  setisLoading(false)
+
+}
+
+
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+<AlertBox></AlertBox>
+<nav className="navbar navbar-expand-lg navbar-dark w-100 bg-dark">
+  <div className="container-fluid">
+
+    <div className="w-100 d-flex" id="navbarSupportedContent">
+    <a className="navbar-brand" href="#">MusicX</a>
+
+      <div className="d-flex w-100" role="search">
+        <input className="form-control me-2" placeholder="eg: dushman hereko here" value={userinput} onChange={(event)=>{setuserinput(event.target.value)}} type="search"  aria-label="Search"/>
+        <button className="btn btn-outline-success"  onClick={getTracks} >Search</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+    </div>
+  </div>
+</nav>
+
+
+      <div className="container">
+<div className={`row py-2 text-center ${isLoading ? '' : 'd-none'}`}>
+
+  <div className="col-12 loaderParent">
+    <div className="capybaraloader">
+  <div className="capybara">
+    <div className="capyhead">
+      <div className="capyear">
+        <div className="capyear2"></div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className="capyear"></div>
+      <div className="capymouth">
+        <div className="capylips"></div>
+        <div className="capylips"></div>
+      </div>
+      <div className="capyeye"></div>
+      <div className="capyeye"></div>
+    </div>
+    <div className="capyleg"></div>
+    <div className="capyleg2"></div>
+    <div className="capyleg2"></div>
+    <div className="capy"></div>
+<p className='loadingText'>  Loading...</p>
+  </div>
+  <div className="loader">
+    <div className="loaderline"></div>
+  </div>
+</div>
+
+  </div>
+</div>
+
+        <div className="row">
+          {
+  tracks.map((track, index) => {
+    return (
+      <div key={track.id} className="col-12 col-md-6 col-lg-3  py-2">
+        <div className="card">
+          <img src={track.album.images[1].url} className="card-img-top" alt={track.title} />
+          <div className="card-body">
+            <h5 className="card-title">{track.name}</h5>
+            <p className="card-text">
+              {track.album.artists[0].name}
+            </p>
+            <p className="card-text"><b>Release Date: </b>
+              {track.album.release_date}
+            </p>
+            <audio src={track.preview_url} controls className='w-100'></audio>
+          </div>
+        </div>
+      </div>
+    );
+  })
+}
+
+        </div>
+      </div>
     </>
   )
 }
